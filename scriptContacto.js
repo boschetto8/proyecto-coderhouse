d1e6e9690d0134b96af1ceeab0acaef123e4541a
Octavio <octavio.boschetto@gmail.com>
2021-06-08 20:59:59 -0300

    commit dom

--- scriptContacto.js
+++ scriptContacto.js
@@ -10,18 +10,55 @@ class Contacto {
 
 }
 
+contactoPrevio = new Contacto ("Juan", "juan@gmail.com","Calle falsa 123", "")
+arrayDeContactos.push(contactoPrevio);
 
 
-let nombre1 = prompt("Cual es su nombre?");
-let email1 = prompt("Cual es su correo electronico?");
-let direccion1 = prompt("Cual es su direccion?");
-let mensaje1 = prompt("Deje un mensaje para la pagina");
-contacto1 = new Contacto (nombre1, email1, direccion1, mensaje1);
 
-agregarArrayDeContacto = (cont) => {
-arrayDeContactos.push (cont)
-};
+    //esta función va a tomar a través de prompt y luego reflejarlos en los input (a futuro la idea es que directamente de los input pase con un boton al storage)
+cargarContacto = () => { 
+const nombre = document.getElementById("nombre");
+const email =  document.getElementById("correo").value;
+const direccion = document.getElementById("direccion").value;
+const mensaje =  document.getElementById("mensaje").value;
 
-agregarArrayDeContacto(contacto1);
+let newContact = new Contacto (nombre, email, direccion, mensaje);
+arrayDeContactos.push(newContact)
+return newContact;
 
-console.log (contacto1);
+}
+
+/*
+
+//acá agregamos el boton que nos va a abrir el prompt para la carga de contactos (a futuro la idea es que suceda directamente con los campos del input)
+let botonCargar = document.createElement("button");
+botonCargar.innerText = "Cargar datos";
+botonCargar.style.border= "4px solid lightblue";
+botonCargar.style.backgroundColor = "grey" 
+botonCargar.type = "button";
+botonCargar.setAttribute("onclick", "cargarContacto()")
+form.appendChild(botonCargar);
+*/
+
+//este boton enviara los datos al storage Local(cuando se cargue a través del input es el campo que quedará). Laura dijo que en la clase entrante veremos el tema de anidar formularios con el storage
+const form = document.getElementById('formu2');
+const botonEnviarContacto = document.createElement("button");
+botonEnviarContacto.innerText = "Enviar";
+botonEnviarContacto.style.border= "4px solid lightblue";
+botonEnviarContacto.style.backgroundColor = "grey" ;
+
+form.appendChild(botonEnviarContacto);
+botonEnviarContacto.setAttribute("onclick", "cargarContacto")
+guardarLocal = (clave, valor) => { localStorage.setItem (clave, valor)};
+
+console.log(arrayDeContactos)
+
+//ESTA FUNCIÓN ENVIA EL DATO DEL CONTACTO AL LOCAL STORAGE (CARGADO ANTERIORMENTE)
+enviarDatosContacto = () => {
+    guardarLocal('Contacto local', JSON.stringify(arrayDeContactos));
+   console.log ()
+
+}
+
+
+
--- scriptLibro.js
+++ scriptLibro.js
@@ -1,12 +1,13 @@
 //constructor de creación de libros
 
 class Libros {
-    constructor (id, nombre, autor, precio, descripcion){
+    constructor (id, nombre, autor, precio, descripcion,imagen){
         this.id = id;
         this.nombre = nombre.toLowerCase();
         this.autor = autor.toLowerCase();
         this.precio = precio;
         this.descripcion = descripcion;
+        this.imagen = imagen
         this.vendido = false
     }
 aumentarLibro (){
@@ -22,99 +23,103 @@ mostrarLibro (){
     console.log ("Este libro "+ this.nombre+ " tiene un valor de $ " +this.precio)
 }
  
- 
-
 }
 
-//creación de libros
-
-const libro1 = new Libros (1, "Mi planta naranja lima", "jose mauro de vasconcelos", 300, "Este libro describe como un niño a través de sus experiencias va asumiendo las consecuencias de su vida" );
-libro1.mostrarLibro();
-
-const libro2 = new Libros (2, "El principito", "Antoine de Saint-Exupéry" , 350, "El principito trata de la historia de un pequeño príncipe que parte de su asteroide a una travesía por el universo, en la cual descubre la extraña forma en que los adultos ven la vida y comprende el valor del amor y la amistad.");
-
-const libro3 = new Libros (3, "Los hermanos Karamazov"," fiodor dotoievski", 260, "Los hermanos Karamazov es una novela filosófica y psicológica ambientada en la Rusia del siglo XIX, que entra profundamente en los debates éticos de Dios, el libre albedrío y la moralidad.");
-
-console.log (libro1)
-console.log (libro2)
-console.log (libro3)
-
-libro2.venderLibro();
-console.log(libro2);
-
-
-arrayLibros = [];
-
-
-agregarLibrosAlArray = (item) => {
-    arrayLibros.push (item)
-};
-agregarLibrosAlArray(libro1);
-agregarLibrosAlArray(libro2);
-agregarLibrosAlArray(libro3);
-
-alert ("Ingresa tu libro así ampliamos nuestra stock")
-let id2 = parseInt(prompt("Ingrese ID de su libro a ingresar"));
-let nombre2 = prompt ("Ingrese nombre de su libro");
-let autor2 = prompt ("Ingrese nombre del autor del libro");
-let precio2= parseFloat(prompt("Ingrese precio de su libro"));
-let descripcion2 = prompt ("Ingrese descripción de su libro");
-
-
-
-let newBook = new Libros (id2, nombre2, autor2, precio2, descripcion2);
-
-console.log (newBook)
-
-
-agregarLibrosAlArray(newBook);
-
-console.log (arrayLibros);
-
-
-// ordenar libros en array de menor precio a mayor
-
-function ordenarArrayLibros() {
-    arrayLibros.sort(function(a, b) {
-        return a.precio - b.precio
-    });
+//Array de libros
+let arrayLibros = [
+    {   id:0, 
+        nombre: "Mi planta naranja lima", 
+        autor:"jose mauro de vasconcelos", 
+        precio: 300, 
+        descripcion:"Este libro describe como un niño a través de sus experiencias va asumiendo las consecuencias de su vida",
+        imagen: 'https://tiendaeureka.com.ar/wp-content/uploads/2020/08/mi-planta-de-naranja-lima.jpg'
+    },
+    {   id:1, 
+        nombre: "El principito", 
+        autor:"Antoine de Saint-Exupéry", 
+        precio: 350, 
+        descripcion:"El principito trata de la historia de un pequeño príncipe que parte de su asteroide a una travesía por el universo, en la cual descubre la extraña forma en que los adultos ven la vida y comprende el valor del amor y la amistad.",
+        imagen: 'https://imagessl4.casadellibro.com/a/l/t7/94/9788478887194.jpg'
+    },
+    {   id:2, 
+        nombre: "Los hermanos Karamazov", 
+        autor:"Fiodor Dotoievski", 
+        precio: 260, 
+        descripcion:"Los hermanos Karamazov es una novela filosófica y psicológica ambientada en la Rusia del siglo XIX, que entra profundamente en los debates éticos de Dios, el libre albedrío y la moralidad.",
+        imagen: 'https://images-na.ssl-images-amazon.com/images/I/51bMDQ4iVsL._SX342_SY445_QL70_ML2_.jpg'
     }
-    
-    ordenarArrayLibros();
-
-    console.log (arrayLibros);
+]
 
 
-const carritoLibros = [];
+//declara array de cargar las compras
+let carritoLibros = [];
 
-agregarCarrito1 = () => {
-carritoLibros.push (libro1)
-};
+arrayLibros.forEach(element => {
+   console.log(element.precio)
+});
 
-agregarCarrito2 = () => {
-    carritoLibros.push (libro2)
-    };
+//Recorremos los libros que tenemos en el ARRAY
+for (item of arrayLibros){
+console.log("del libro " +item.nombre+ " el ID es " +item.id+ " y tiene un precio de $" +item.precio)}
 
-    agregarCarrito3 = () => {
-        carritoLibros.push (libro3)
-        };
+console.log(arrayLibros)
+
+//Tomamos del HTML un ID para generar etiquetas dinamicas
+const seccion = document.getElementById("articulos");
+//A través de un ForEach vamos creando dinamicamente todos los componentes/cards
+arrayLibros.forEach (libro => {
+const crearArticulo = document.createElement("div");
+crearArticulo.innerHTML = `
+            <div class="card-wrapper">
+                <div class="row align-items-center">
+                    <div class="col-12 col-md-4">
+                        <div class="image-wrapper">
+                            <img src="${libro.imagen}" alt="${libro.nombre}">
+                        </div>
+                    </div>
+                    <div class="col-12 col-md">
+                        <div class="card-box">
+                            <div class="row">
+                                <div class="col-md">
+                                    <h6 class="card-title mbr-fonts-style display-5">
+                                        <strong>${libro.nombre}</strong></h6>
+                                    <p class="mbr-text mbr-fonts-style display-7">${libro.descripcion}</p>
+                                </div>
+                                <div class="col-md-auto">
+                                    <p class="price mbr-fonts-style display-2">$${libro.precio}</p>
+                                    <div class="mbr-section-btn"><a class="btn btn-primary display-4" id="id-${libro.id}">Comprar</a></div>
+                                </div>`;                              
+articulos.appendChild(crearArticulo);
+
+//Función para agregar items al carrito (declarando la función fuera o dentro del AddEvent me daba error y no me cargaba los componentes siguientes por eso lo cargue de esta forma)
+let botonComprar = document.getElementById ("id-"+libro.id);
+botonComprar.addEventListener('click', () => {
+    console.log("Se agrego al carrito el libro "+libro.nombre +" que tiene un precio de $ "+libro.precio )
+    carritoLibros.push(libro)});
+       
+ // constante para crear en el storage un session de la compra
+    const guardarTemporal = (clave, valor) => {sessionStorage.setItem(clave, valor)};
+//Agregar al Session Storage los libros del carrito
+    librosAgregados = () => {
+        guardarTemporal ('book', JSON.stringify(carritoLibros))
+    }
+    })
+
+
 verCarrito = () => {
     let total = 0;
-    for (item of carritoLibros){
-        console.log ("nombre del libro: " +item.nombre+ " tiene un costo de $ "+item.precio )
-        total += item.precio;
-        }
+    carritoLibros.forEach ( e => { 
+        total += e.precio;})       
+
         if (total>1000){
          let descuentoCompra = total*0.90 ; //forma de calcular el 10% en lugar de regla de 3 simples
             console.log ("Por el monto de tu compra te hicimos un descuento del 10% quedando el total $ " + descuentoCompra)
-        } else{
+                        } 
+        else{
 console.log ("El precio total de su compra es $ "+total )
-}}
-
-
-
-
-
+                }
+    librosAgregados();
+    }
 
 
 
